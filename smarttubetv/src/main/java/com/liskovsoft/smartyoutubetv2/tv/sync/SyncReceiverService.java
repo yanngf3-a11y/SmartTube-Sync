a140052facbee7ac2377153d34f156aa04e942e7
yanngf3-a11y <yanngf3@gmail.com>
2026-09-03 16:47:03 -0600

    Update SyncReceiverService.java

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/sync/SyncReceiverService.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/sync/SyncReceiverService.java
@@ -6,50 +6,131 @@ import android.os.IBinder;
 import android.util.Log;
 
 /**
- * Servicio Android que mantiene vivo el SyncWebSocketServer mientras la pantalla
- * está lista para recibir comandos de sincronización desde el teléfono.
- */
-public class SyncReceiverService extends Service {
-    private static final String TAG = SyncReceiverService.class.getSimpleName();
-    private static final int SYNC_PORT = 8765;
 
-    private SyncWebSocketServer mServer;
+* Servicio Android que mantiene activo el servidor WebSocket
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+* de YG Sync desde el arranque de SmartTube.
 
-        SyncPlayerBridge bridge = new SyncPlaybackBridge(getApplicationContext());
-        mServer = new SyncWebSocketServer(SYNC_PORT, bridge);
+* 
 
-        try {
-            mServer.start();
-        } catch (Exception e) {
-            Log.e(TAG, "No se pudo iniciar el servidor de sincronizacion", e);
-        }
-    }
+* Transporte:
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // START_STICKY: si Android mata el servicio, intenta recrearlo.
-        return START_STICKY;
-    }
+* 
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+* TCP 8765 = WebSocket
 
-        if (mServer != null) {
-            try {
-                mServer.stop();
-            } catch (Exception e) {
-                Log.e(TAG, "Error al detener el servidor de sincronizacion", e);
-            }
-        }
-    }
+* 
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-}
+* UDP 8766 = descubrimiento de dispositivos
+  */
+  public class SyncReceiverService extends Service {
+  
+  private static final String TAG =
+  SyncReceiverService.class.getSimpleName();
+  
+  private static final int SYNC_PORT =
+  8765;
+  
+  private SyncWebSocketServer mServer;
+  
+  @Override
+  public void onCreate() {
+  
+   super.onCreate();
+
+ Log.i(
+         TAG,
+         "YG Sync: iniciando servicio"
+ );
+
+ try {
+
+     SyncPlayerBridge bridge =
+             new SyncPlaybackBridge(
+                     getApplicationContext()
+             );
+
+     mServer =
+             new SyncWebSocketServer(
+                     SYNC_PORT,
+                     bridge
+             );
+
+     mServer.start();
+
+     Log.i(
+             TAG,
+             "YG Sync: WebSocket iniciado en TCP "
+                     + SYNC_PORT
+     );
+
+ } catch (Exception e) {
+
+     Log.e(
+             TAG,
+             "YG Sync: ERROR iniciando WebSocket",
+             e
+     );
+
+     mServer =
+             null;
+ }
+  
+  }
+  
+  @Override
+  public int onStartCommand(
+  Intent intent,
+  int flags,
+  int startId
+  ) {
+  
+   Log.i(
+         TAG,
+         "YG Sync: servicio activo"
+ );
+
+ return START_STICKY;
+  
+  }
+  
+  @Override
+  public void onDestroy() {
+  
+   Log.i(
+         TAG,
+         "YG Sync: deteniendo servicio"
+ );
+
+ if (mServer != null) {
+
+     try {
+
+         mServer.stop();
+
+     } catch (Exception e) {
+
+         Log.e(
+                 TAG,
+                 "YG Sync: error deteniendo WebSocket",
+                 e
+         );
+     }
+
+     mServer =
+             null;
+ }
+
+ super.onDestroy();
+  
+  }
+  
+  @Override
+  public IBinder onBind(
+  Intent intent
+  ) {
+  
+   return null;
+  
+  }
+  }
